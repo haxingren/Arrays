f34ad5f7d235d5c2bcf6996b20becfc1a75ad275
haxingren <tanjingyuan17@gmail.com>
2024-08-06 17:59:11 +0800

    Add files via upload

---
+++ 数组练习2 找最大值与最低值之差.c
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[100] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int max = arr[0];
+//	int min = arr[0];
+//	for (i = 1; i < n; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//		if (min > arr[i])
+//		{
+//			min = arr[i];
+//		}
+//
+//	}
+//	printf("%d\n", (max - min));
+//
+//	return 0;
+//}
+
+
+//简化版本
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int arr[100] = { 0 };
+	int i = 0;
+	int max = 0;
+	int min = 100;
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+		if (arr[i] > max)
+			max = arr[i];
+		if (arr[i] < min)
+			min = arr[i];
+	}
+	printf("%d\n", (max - min));
+
+	return 0;
+}
